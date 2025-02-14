ec8f5793e3df8a39549da53394dd576af86a4b5f
Syafiq Basri <altsyafiq@gmail.com>
2025-02-14 22:42:07 +0800

    feat: added disabled style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,6 +22,7 @@ const Button: React.FC<ButtonProps> = ({
 				'font-semibold transition-all duration-150',
 				'inline-flex items-center flex-shrink-0 gap-x-1.5',
 				'focus:outline-none focus-visible:outline-none focus:ring-2',
+				'disabled:cursor-not-allowed disabled:opacity-30',
 				{
 					// size
 					'px-2.5 py-1.5 text-sm rounded-lg': size === 'xs',
@@ -31,17 +32,17 @@ const Button: React.FC<ButtonProps> = ({
 
 					// solid
 					'text-white': appearance === 'solid',
-					'bg-primary-500 hover:bg-primary-600 focus:ring-primary-500/30': appearance === 'solid' && variant === 'primary',
-					'bg-secondary-500 hover:bg-secondary-600 focus:ring-secondary-500/30': appearance === 'solid' && variant === 'secondary',
-					'bg-success-500 hover:bg-success-600 focus:ring-success-500/30': appearance === 'solid' && variant === 'success',
-					'bg-error-500 hover:bg-error-600 focus:ring-error-500/30': appearance === 'solid' && variant === 'error',
+					'bg-primary-500 enabled:hover:bg-primary-600 enabled:focus:ring-primary-500/30': appearance === 'solid' && variant === 'primary',
+					'bg-secondary-500 enabled:hover:bg-secondary-600 enabled:focus:ring-secondary-500/30': appearance === 'solid' && variant === 'secondary',
+					'bg-success-500 enabled:hover:bg-success-600 enabled:focus:ring-success-500/30': appearance === 'solid' && variant === 'success',
+					'bg-error-500 enabled:hover:bg-error-600 enabled:focus:ring-error-500/30': appearance === 'solid' && variant === 'error',
 
 					// outline
-					'bg-transparent border hover:text-white': appearance === 'outline',
-					'text-primary-500 border-primary-500 hover:bg-primary-500 focus:ring-primary-500/40': appearance === 'outline' && variant === 'primary',
-					'text-secondary-500 border-secondary-500 hover:bg-secondary-500 focus:ring-secondary-500/40': appearance === 'outline' && variant === 'secondary',
-					'text-success-500 border-success-500 hover:bg-success-500 focus:ring-success-500/40': appearance === 'outline' && variant === 'success',
-					'text-error-500 border-error-500 hover:bg-error-500 focus:ring-error-500/40': appearance === 'outline' && variant === 'error',
+					'bg-transparent border enabled:hover:text-white': appearance === 'outline',
+					'text-primary-500 border-primary-500 enabled:hover:bg-primary-500 enabled:focus:ring-primary-500/40': appearance === 'outline' && variant === 'primary',
+					'text-secondary-500 border-secondary-500 enabled:hover:bg-secondary-500 enabled:focus:ring-secondary-500/40': appearance === 'outline' && variant === 'secondary',
+					'text-success-500 border-success-500 enabled:hover:bg-success-500 enabled:focus:ring-success-500/40': appearance === 'outline' && variant === 'success',
+					'text-error-500 border-error-500 enabled:hover:bg-error-500 enabled:focus:ring-error-500/40': appearance === 'outline' && variant === 'error',
 				},
 				className,
 			)}>
