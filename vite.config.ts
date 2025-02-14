4f45385bd12f3282ce5ee6360d2159e3cf8d3a46
Syafiq Basri <altsyafiq@gmail.com>
2025-02-14 23:23:18 +0800

    build: configured prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -7,5 +8,23 @@ export default defineConfig({
 	plugins: [
 		react(),
 		tailwindcss(),
+		dts({ insertTypesEntry: true }),
 	],
+	build: {
+		lib: {
+			entry: 'src/entrypoint.ts',
+			name: 'PieKit',
+			formats: ['es', 'cjs', 'umd'],
+			fileName: (format) => `piekit.${ format }.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
 })
