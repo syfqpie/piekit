080ad117c6bb3cb67aea382c67bb26a1c561d896
Syafiq Basri <altsyafiq@gmail.com>
2025-02-14 22:27:54 +0800

    feat: added outline opt

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,7 @@ import { ButtonHTMLAttributes } from 'react'
 import clsx from 'clsx'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	appearance?: 'solid';
+	appearance?: 'solid' | 'outline';
 	size?: 'xs' | 'sm' | 'md' | 'lg';
 	variant?: 'primary' | 'secondary' | 'success' | 'error';
 }
@@ -19,7 +19,7 @@ const Button: React.FC<ButtonProps> = ({
 		<button
 			{...props}
 			className={clsx(
-				'font-semibold transition-colors duration-150',
+				'font-semibold transition-all duration-150',
 				'inline-flex items-center flex-shrink-0 gap-x-1.5',
 				'focus:outline-none focus-visible:outline-none focus:ring-2',
 				{
@@ -30,10 +30,18 @@ const Button: React.FC<ButtonProps> = ({
 					'px-6 py-3 text-lg rounded-xl': size === 'lg',
 
 					// solid
-					'text-white bg-primary-500 hover:bg-primary-600 focus:ring-primary-500/30': appearance === 'solid' && variant === 'primary',
-					'text-white bg-secondary-500 hover:bg-secondary-600 focus:ring-secondary-500/30': appearance === 'solid' && variant === 'secondary',
-					'text-white bg-success-500 hover:bg-success-600 focus:ring-success-500/30': appearance === 'solid' && variant === 'success',
-					'text-white bg-error-500 hover:bg-error-600 focus:ring-error-500/30': appearance === 'solid' && variant === 'error',
+					'text-white': appearance === 'solid',
+					'bg-primary-500 hover:bg-primary-600 focus:ring-primary-500/30': appearance === 'solid' && variant === 'primary',
+					'bg-secondary-500 hover:bg-secondary-600 focus:ring-secondary-500/30': appearance === 'solid' && variant === 'secondary',
+					'bg-success-500 hover:bg-success-600 focus:ring-success-500/30': appearance === 'solid' && variant === 'success',
+					'bg-error-500 hover:bg-error-600 focus:ring-error-500/30': appearance === 'solid' && variant === 'error',
+
+					// outline
+					'bg-transparent border hover:text-white': appearance === 'outline',
+					'text-primary-500 border-primary-500 hover:bg-primary-500 focus:ring-primary-500/40': appearance === 'outline' && variant === 'primary',
+					'text-secondary-500 border-secondary-500 hover:bg-secondary-500 focus:ring-secondary-500/40': appearance === 'outline' && variant === 'secondary',
+					'text-success-500 border-success-500 hover:bg-success-500 focus:ring-success-500/40': appearance === 'outline' && variant === 'success',
+					'text-error-500 border-error-500 hover:bg-error-500 focus:ring-error-500/40': appearance === 'outline' && variant === 'error',
 				},
 				className,
 			)}>
