27982cd064c8872659f3076d4296279af592b93b
Syafiq Basri <altsyafiq@gmail.com>
2025-02-15 14:05:49 +0800

    feat: improved section style

--- src/layout/sandbox/SandboxSection.tsx
+++ src/layout/sandbox/SandboxSection.tsx
@@ -9,7 +9,7 @@ const SandboxSection: React.FC<SandboxSectionProps> = ({ title, children }) => {
 	return (
 		<section className='mb-2'>
 			<h3 className='mb-1.5 font-semibold'>{title}</h3>
-			<div>{children}</div>
+			<div className='bg-gray-100 p-6 min-w-3xl rounded-lg'>{children}</div>
 		</section>
 	)
 }
