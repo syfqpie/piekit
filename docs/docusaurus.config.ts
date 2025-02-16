5eb544b71748d753eb1bb30eb5351e519958fe88
Syafiq Basri <altsyafiq@gmail.com>
2025-02-16 20:22:42 +0800

    docs: bump version; configured metadata

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
 	favicon: 'img/favicon.png',
 
 	// Set the production url of your site here
-	url: 'https://piekit.vercel.app',
+	url: 'https://piekit.cendol.dev',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
