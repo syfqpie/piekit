46db5358baa32192d4a8fd5e31339bbbeccfa011
Syafiq Basri <altsyafiq@gmail.com>
2025-03-25 02:36:30 +0800

    docs: update meta

--- lib/components/FormInput/FormInput.stories.ts
+++ lib/components/FormInput/FormInput.stories.ts
@@ -5,7 +5,38 @@ const meta: Meta<typeof FormInput> = {
 	title: 'Components/FormInput',
 	component: FormInput,
 	args: { },
-	argTypes: { },
+	argTypes: {
+		disabled: {
+			name: 'Disabled',
+			description: 'Set input disabled state',
+			control: 'boolean',
+		},
+		label: {
+			name: 'Label',
+			description: 'Form label',
+			control: 'text',
+		},
+		placeholder: {
+			name: 'Placeholder',
+			description: 'Form placeholder',
+			control: 'text',
+		},
+		testId: {
+			name: 'Test ID',
+			description: 'Component base test ID',
+			control: 'text',
+		},
+		hasError: {
+			name: 'Has error',
+			description: 'Set input error state',
+			control: 'boolean',
+		},
+		errorMessages: {
+			name: 'Error messages',
+			description: 'Set input error state',
+			control: 'object',
+		},
+	},
 }
 
 export default meta
--- lib/components/FormTextArea/FormTextArea.stories.ts
+++ lib/components/FormTextArea/FormTextArea.stories.ts
@@ -5,7 +5,38 @@ const meta: Meta<typeof FormTextArea> = {
 	title: 'Components/FormTextArea',
 	component: FormTextArea,
 	args: { },
-	argTypes: { },
+	argTypes: {
+		disabled: {
+			name: 'Disabled',
+			description: 'Set text area disabled state',
+			control: 'boolean',
+		},
+		label: {
+			name: 'Label',
+			description: 'Form label',
+			control: 'text',
+		},
+		placeholder: {
+			name: 'Placeholder',
+			description: 'Form placeholder',
+			control: 'text',
+		},
+		testId: {
+			name: 'Test ID',
+			description: 'Component base test ID',
+			control: 'text',
+		},
+		hasError: {
+			name: 'Has error',
+			description: 'Set text area error state',
+			control: 'boolean',
+		},
+		errorMessages: {
+			name: 'Error messages',
+			description: 'Set text area error state',
+			control: 'object',
+		},
+	},
 }
 
 export default meta
