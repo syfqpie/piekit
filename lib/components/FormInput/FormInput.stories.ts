56984c189b3dbca8115d089a9443cbb090778650
Syafiq Basri <altsyafiq@gmail.com>
2025-03-31 16:02:40 +0800

    docs: added FormInput docs and improved FormInput story

--- lib/components/FormInput/FormInput.stories.ts
+++ lib/components/FormInput/FormInput.stories.ts
@@ -22,8 +22,8 @@ const meta: Meta<typeof FormInput> = {
 			control: 'text',
 		},
 		testId: {
-			name: 'Test ID',
-			description: 'Component base test ID',
+			name: 'Test automation tag',
+			description: 'Test automation ID',
 			control: 'text',
 		},
 		hasError: {
@@ -43,26 +43,46 @@ export default meta
 
 type Story = StoryObj<typeof FormInput>
 
-export const Default: Story = {
+export const LabelOnly: Story = {
 	args: {
-		testId: 'default-form',
 		label: 'Name',
+	},
+}
+
+export const PlaceholderOnly: Story = {
+	args: {
 		placeholder: 'Enter name',
 	},
 }
 
 export const Disabled: Story = {
 	args: {
-		testId: 'disabled-form',
 		label: 'Name',
 		placeholder: 'Enter name',
 		disabled: true,
 	},
 }
 
-export const ErrorState: Story = {
+export const ErrorStateWithoutMessage: Story = {
 	args: {
-		testId: 'error-form',
+		label: 'Name',
+		placeholder: 'Enter name',
+		hasError: true,
+	},
+}
+
+export const ErrorStateWithMessage: Story = {
+	args: {
+		label: 'Name',
+		placeholder: 'Enter name',
+		hasError: true,
+		errorMessages: ['Field required'],
+	},
+}
+
+export const WithTA: Story = {
+	args: {
+		testId: 'name-form-ta',
 		label: 'Name',
 		placeholder: 'Enter name',
 		hasError: true,
