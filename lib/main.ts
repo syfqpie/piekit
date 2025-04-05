8ab1067fc73d57a8af4a9d6fa9c5cd3e97fd8242
Syafiq Basri <altsyafiq@gmail.com>
2025-04-05 20:53:10 +0800

    chore: updated export

--- lib/main.ts
+++ lib/main.ts
@@ -2,12 +2,16 @@ import './styles.css'
 
 export { Button } from './components/Button'
 export { Card } from './components/Card'
+export { Dropdown } from './components/Dropdown'
 export { FormInput } from './components/FormInput'
+export { FormSelect } from './components/FormSelect'
 export { FormTextArea } from './components/FormTextArea'
 
 export type { ButtonProps } from './types/button'
 export type { CardProps } from './types/card'
+export type { DropdownProps } from './types/dropdown'
 export type { FormInputProps } from './types/forminput'
 export type { FormTextAreaProps } from './types/formtextarea'
+export type { FormSelectProps } from './types/formselect'
 
 export { useOuterClick } from './hooks/useOuterClick'
